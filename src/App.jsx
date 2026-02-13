936db3c2f54d3e284bedce0a4a9cd6a35754c131
abdrezakkemal@gmail.com <abdrezakkemal@gmail.com>
2026-02-13 09:07:51 +0200

    feat: add Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import Home from "./Pages/Home/Home";
 
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>Netflix Clone</h1>
+      <Home />
     </>
   );
 }
