bee51248604671d6efb005a6e2c3994faeb85478
abdrezakkemal@gmail.com <abdrezakkemal@gmail.com>
2026-02-19 16:54:33 +0300

    feat: adding header and footer

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import "./Header.css";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
+const Header = () => {
+  return (
+    <div className="header-outer-container">
+      <div className="header-container">
+        <div className="header-left">
+          <ul>
+            <li>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
+                alt="Netflix Logo"
+                width={100}
+              />
+            </li>
+            <li>Home</li>
+            <li>TV Shows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>My List</li>
+            <li>Browse by Languages</li>
+          </ul>
+        </div>
+        <div className="header-right">
+          <ul>
+            <li>
+              <SearchIcon />
+            </li>
+            <li>
+              <NotificationsNoneIcon />
+            </li>
+            <li>
+              <AccountBoxIcon />
+            </li>
+            <li>
+              <ArrowDropDownIcon />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
